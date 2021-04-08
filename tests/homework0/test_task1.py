ba748a061620278d8e14bd555bc343dd480a1c8c
Ilya Samartsev <ilya.samartsev@exness.com>
2021-04-08 15:16:13 +0300

    project structure

---
+++ tests/homework0/test_task1.py
@@ -0,0 +1,5 @@
+from homework0.task1 import some_function
+
+
+def test_some_function():
+    assert some_function()
---
+++ tests/homework0/test_task2.py
@@ -0,0 +1,5 @@
+from homework0.task2 import another_function
+
+
+def test_another_function():
+    assert another_function() == "OK"
