e9c2cf81995da3dd3a6d728342f91cd32f26a103
ErshovVladislav10M <yersh2000@gmail.com>
2021-11-10 01:23:28 +0300

    Update test_task02.py

--- homework1/tests/test_task02.py
+++ homework1/tests/test_task02.py
@@ -4,9 +4,9 @@ from taks.task02 import check_fibonacci
 
 @pytest.mark.parametrize(
     "input_data, output_data",
-    [([0, 1, 1, 2, 3], True), 
-     ([0, 1], True), 
-     ([1, 0, 1], False), 
+    [([0, 1, 1, 2, 3], True),
+     ([0, 1], True),
+     ([1, 0, 1], False),
      ([0], False),
      ([-1, 0, 1], False)]
 )
