abb1412ed37b21d4bd9308b6cb37a7971b382b84
ErshovVladislav10M <yersh2000@gmail.com>
2021-11-09 03:34:24 +0300

    update test task1 and task2

--- tests/homework1/test_task2.py
+++ tests/homework1/test_task2.py
@@ -1,22 +1,11 @@
-import pytest
-
-
 from homework1.task2 import check_fibonacci
 
 
-@pytest.mark.parametrize(
-        'test_input, expected',
-        [[[0, 1, 1, 2, 3], True], [[0, 1], True]]
-)
 def test_positive_case(test_input, expected):
     """Testing that actual powers of 2 give True"""
-    assert check_fibonacci(test_input) == expected
+    assert check_fibonacci([0, 1, 1, 2, 3])
 
 
-@pytest.mark.parametrize(
-        'test_input, expected',
-        [[[0, 1, 1, 2, 6], False], [[0], False]]
-)
 def test_negative_case(test_input, expected):
     """Testing that non-powers of 2 give False"""
-    assert check_fibonacci(test_input) == expected
+    assert check_fibonacci([0, 1, 1, 2, 6])
