d83ced0e1c736ea412705068babb4387b85969cc
ErshovVladislav10M <yersh2000@gmail.com>
2021-11-10 01:13:13 +0300

    Update test_calculator.py

--- homework1/sample_project/tests/test_calculator.py
+++ homework1/sample_project/tests/test_calculator.py
@@ -1,9 +1,9 @@
 import pytest
 
-
 from calculator.calc import check_power_of_2
 
 
-@pytest.mark.parametrize("num, output", [(65536, True), (-17, False)])
+@pytest.mark.parametrize("num, output",
+                         [(1, True), (65536, True), (3, False), (-17, False)])
 def test_check_power_of_2(num, output):
     assert check_power_of_2(num) == output
