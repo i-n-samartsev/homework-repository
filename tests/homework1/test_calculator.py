6532b9883ebe56bdb7fcd47d9712f3ef5b8bff99
ErshovVladislav10M <yersh2000@gmail.com>
2021-11-09 03:38:17 +0300

    Update test_calculator.py

--- tests/homework1/test_calculator.py
+++ tests/homework1/test_calculator.py
@@ -1,11 +1,11 @@
-from homework1.calc import check_power_of_2
+from calculator.calc import check_power_of_2
 
 
-def test_positive_case(test_input, expected):
+def test_positive_case():
     """Testing that actual powers of 2 give True"""
     assert check_power_of_2(65536)
 
 
-def test_negative_case(test_input, expected):
+def test_negative_case():
     """Testing that non-powers of 2 give False"""
-    assert check_power_of_2(12)
+    assert not check_power_of_2(12)
