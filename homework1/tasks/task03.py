f110d470ac9ed99327ae905cce5b62896ebbcfba
ErshovVladislav10M <yersh2000@gmail.com>
2021-11-09 19:03:06 +0300

    ++ task text

--- homework1/tasks/task03.py
+++ homework1/tasks/task03.py
@@ -1,3 +1,19 @@
+"""
+Write down the function, which reads input line-by-line,
+and find maximum and minimum values.
+Function should return a tuple with the max and min values.
+
+For example for [1, 2, 3, 4, 5], function should return [1, 5]
+
+We guarantee, that file exists and contains line-delimited integers.
+
+To read file line-by-line you can use this snippet:
+
+with open("some_file.txt") as fi:
+    for line in fi:
+        ...
+
+"""
 from typing import Tuple
 
 
--- homework1/tasks/task04.py
+++ homework1/tasks/task04.py
@@ -1,3 +1,13 @@
+"""
+Classic task, a kind of walnut for you
+
+Given four lists A, B, C, D of integer values,
+    compute how many tuples (i, j, k, l) there are
+    such that A[i] + B[j] + C[k] + D[l] is zero.
+
+We guarantee, that all A, B, C, D have same
+length of N where 0 ≤ N ≤ 1000.
+"""
 from typing import List
 
 
