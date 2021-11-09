7574da162f42e2c450bfda369d16b8528f1f1f4e
ErshovVladislav10M <yersh2000@gmail.com>
2021-11-09 03:11:20 +0300

    Update test in task1

--- homework1/sample_project/tests/test_calculator.py
+++ homework1/sample_project/tests/test_calculator.py
@@ -1,12 +1,22 @@
-from calculator.calc import check_power_of_2
+import pytest
 
 
-def test_positive_case():
+from homework1.task1 import check_power_of_2
+
+
+@pytest.mark.parametrize(
+        'test_input, expected',
+        [(65536, True), [16, True], [2, True]]
+)
+def test_positive_case(test_input, expected):
     """Testing that actual powers of 2 give True"""
-    assert check_power_of_2(65536)
+    assert check_power_of_2(test_input) == expected
 
 
-def test_negative_case():
+@pytest.mark.parametrize(
+        'test_input, expected',
+        [(12, False), [5, False], [-1, False]]
+)
+def test_negative_case(test_input, expected):
     """Testing that non-powers of 2 give False"""
-    assert not check_power_of_2(12)
-    assert not check_power_of_2(-3)
+    assert check_power_of_2(test_input) == expected
