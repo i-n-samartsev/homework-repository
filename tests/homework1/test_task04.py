440a8714002e6941493e7f7cfaf59960bb901fdd
ErshovVladislav10M <yersh2000@gmail.com>
2021-11-11 02:39:05 +0300

    Update test_task04.py

--- tests/homework1/test_task04.py
+++ tests/homework1/test_task04.py
@@ -4,8 +4,9 @@ from homework1.task04 import check_sum_of_four
 
 
 @pytest.mark.parametrize(
-    "input_data, output_data",
-    [(([0, 1], [1, 2], [2, 3], [-2, -4]), 3)]
+    "input_d1, input_d2, input_d3, input_d4, output_d",
+    [([0, 1], [1, 2], [2, 3], [-2, -4], 3)]
 )
 def test_check_sum_of_four(input_data, output_data):
-    assert check_sum_of_four(input_data) == output_data
+    assert check_sum_of_four(input_d1, input_d2,
+                             input_d3, input_d4) == output_d
