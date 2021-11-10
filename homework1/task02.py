e8ef0fe703f7e2b3eb9f6a6958115b01e2835f7d
ErshovVladislav10M <yersh2000@gmail.com>
2021-11-11 02:56:45 +0300

    Update task02.py

--- homework1/task02.py
+++ homework1/task02.py
@@ -12,8 +12,9 @@ from typing import Sequence
 def check_fibonacci(data: Sequence[int]) -> bool:
     if len(data) < 2:
         return False
-    if data[0] == 0 and data[1] == 1:
-        for i in range(0, len(data) - 2):
-            if data[i] + data[i + 1] != data[i + 2]:
-                return False
+    if data[0] != 0 or data[1] != 1:
+        return False
+    for i in range(0, len(data) - 2):
+        if data[i] + data[i + 1] != data[i + 2]:
+            return False
     return True
