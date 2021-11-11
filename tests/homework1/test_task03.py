db12a365dc38ea79d081ad07ace2656a5869fdae
ErshovVladislav10M <yersh2000@gmail.com>
2021-11-11 03:40:00 +0300

    Create tets_task03

---
+++ tests/homework1/test_task03.py
@@ -0,0 +1,13 @@
+import pytest
+
+from homework1.task03 import find_maximum_and_minimum
+
+
+@pytest.mark.parametrize(
+    "input_data, output_data",
+    [(test_task03_data, (3, 56))]
+)
+def find_maximum_and_minimum(input_data, output_data):
+    nums, k = input_data
+    min_val, max_val = output_data
+    assert find_maximal_subarray_sum(nums, k) == output_data
