54d6181ea20b4242a3b3b14fb4050b03029bd256
ErshovVladislav10M <yersh2000@gmail.com>
2021-11-11 03:32:04 +0300

    Update task04.py

--- homework1/task04.py
+++ homework1/task04.py
@@ -8,16 +8,15 @@ Given four lists A, B, C, D of integer values,
 We guarantee, that all A, B, C, D have same
 length of N where 0 ≤ N ≤ 1000.
 """
+import itertools
+
 from typing import List
 
 
 def check_sum_of_four(a: List[int], b: List[int],
                       c: List[int], d: List[int]) -> int:
     counter = 0
-    for i in a:
-        for j in b:
-            for k in c:
-                for t in d:
-                    if i + j + k + t == 0:
-                        counter += 1
+    for i in itertools.product(a, b, c, d):
+        if sum(i) == 0:
+            counter += 1
     return counter
