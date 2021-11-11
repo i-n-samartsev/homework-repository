b0fa9a8b8b3d4cd15987decbf2323c29385d6c63
ErshovVladislav10M <yersh2000@gmail.com>
2021-11-11 03:42:06 +0300

    Update test_task03.py

--- tests/homework1/test_task03.py
+++ tests/homework1/test_task03.py
@@ -7,6 +7,6 @@ from homework1.task03 import find_maximum_and_minimum
     "input_data, output_data",
     [("test_task03_data", (3, 56))]
 )
-def find_maximum_and_minimum(input_data, output_data):
+def test_find_maximum_and_minimum(input_data, output_data):
     min_val, max_val = output_data
     assert find_maximal_subarray_sum(input_data) == output_data
