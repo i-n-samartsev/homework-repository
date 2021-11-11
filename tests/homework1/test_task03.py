d6dde8fde89855623f33eb8d46076c99606bb58d
ErshovVladislav10M <yersh2000@gmail.com>
2021-11-12 01:50:41 +0300

    Update test_task03.py

--- tests/homework1/test_task03.py
+++ tests/homework1/test_task03.py
@@ -1,6 +1,7 @@
+import os
+
 from homework1.task03 import find_maximum_and_minimum
 
 
 def test_find_maximum_and_minimum():
-    assert find_maximum_and_minimum(
-        r"homework-repository\homework1\test_task03_data.txt") == (3, 56)
+    assert find_maximum_and_minimum(os.getcwd() + "\\" + "test_task03_data.txt") == (3, 56)
