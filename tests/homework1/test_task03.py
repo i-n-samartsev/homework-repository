91bc0b2a892da262b8d6755f360d1d8bb9f84b12
ErshovVladislav10M <yersh2000@gmail.com>
2021-11-11 04:12:22 +0300

    wtf?

--- tests/homework1/test_task03.py
+++ tests/homework1/test_task03.py
@@ -5,7 +5,7 @@ from homework1.task03 import find_maximum_and_minimum
 
 @pytest.mark.parametrize(
     "input_data, output_data",
-    [(r'homework1\test_task03_data.txt', (3, 56))]
+    [("test_task03_data.txt", (3, 56))]
 )
 def test_find_maximum_and_minimum(input_data, output_data):
     min_val, max_val = output_data
