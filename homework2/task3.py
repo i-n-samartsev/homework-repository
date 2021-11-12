c7ad83546f489def02ea0814ac2603bf68f8de8b
ErshovVladislav10M <yersh2000@gmail.com>
2021-11-12 14:59:38 +0300

    create tasks

---
+++ homework2/task3.py
@@ -0,0 +1,25 @@
+"""
+Write a function that takes K lists as arguments and returns all possible
+lists of K items where the first element is from the first list,
+the second is from the second and so one.
+
+You may assume that that every list contain at least one element
+
+Example:
+
+assert combinations([1, 2], [3, 4]) == [
+    [1, 3],
+    [1, 4],
+    [2, 3],
+    [2, 4],
+]
+"""
+import itertools
+from typing import Any, List
+
+
+def combinations(*args: List[Any]) -> List[List]:
+    ans = []
+    for i in itertools.product(args):
+        ans.append(i[0])
+    return ans
