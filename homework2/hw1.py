8e0c3c1c8aa0bedf09e68debb0f39d7a89dd54bf
zhuchkov-artem <artem-1102@mail.ru>
2021-11-13 22:12:00 +0300

    Homework # 2 completed

--- homework2/hw1.py
+++ homework2/hw1.py
@@ -7,23 +7,82 @@ Given a file containing text. Complete using only default collections:
     5) Find most common non ascii char for document
 """
 from typing import List
+from collections import Counter, defaultdict
+import string
 
 
 def get_longest_diverse_words(file_path: str) -> List[str]:
-    ...
+    words_stat = defaultdict(list)
+
+    with open(file_path, mode='r', encoding='unicode-escape',
+              errors='ignore') as file:
+        for line in file:
+            words = line.split()
+            for word in words:
+                words_stat[len(word)].append(word.strip(string.punctuation))
+
+    sorted_length = sorted(words_stat, reverse=True)
+    answer = []
+
+    for length in sorted_length:
+        if not words_stat[length]:
+            continue
+        answer.append(words_stat[length].pop())
+        if len(answer) > 9:
+            break
+
+    return answer
 
 
 def get_rarest_char(file_path: str) -> str:
-    ...
+    chars_stat = Counter()
+
+    with open(file_path, mode='r', encoding='unicode-escape',
+              errors='ignore') as file:
+        for line in file:
+            for char in line:
+                chars_stat[char] += 1
+
+    return chars_stat.most_common()[-1][0]
 
 
 def count_punctuation_chars(file_path: str) -> int:
-    ...
+    number_of_punctuation_chars = 0
+
+    with open(file_path, mode='r', encoding='unicode-escape',
+              errors='ignore') as file:
+        for line in file:
+            for char in line:
+
+                if char in string.punctuation:
+                    number_of_punctuation_chars += 1
+
+    return number_of_punctuation_chars
 
 
 def count_non_ascii_chars(file_path: str) -> int:
-    ...
+    number_of_non_ascii_chars = 0
+
+    with open(file_path, mode='r', encoding='unicode-escape',
+              errors='ignore') as file:
+        for line in file:
+            for char in line:
+
+                if ord(char) > 127:
+                    number_of_non_ascii_chars += 1
+
+    return number_of_non_ascii_chars
 
 
 def get_most_common_non_ascii_char(file_path: str) -> str:
-    ...
+    non_ascii_chars_stat = Counter()
+
+    with open(file_path, mode='r', encoding='unicode-escape',
+              errors='ignore') as file:
+        for line in file:
+            for char in line:
+
+                if ord(char) > 127:
+                    non_ascii_chars_stat[char] += 1
+
+    return non_ascii_chars_stat.most_common()[0][0]
--- tests/homework2/test_hw1.py
+++ tests/homework2/test_hw1.py
@@ -4,3 +4,56 @@ from homework2.hw1 import count_punctuation_chars
 from homework2.hw1 import count_non_ascii_chars
 from homework2.hw1 import get_most_common_non_ascii_char
 
+
+def test_get_longest_diverse_words():
+    with open('test_file.txt', mode='w', encoding='unicode-escape') as file:
+        file.write('1 12 123 1234 12345 \n')
+        file.write('123456 1234567  12345678 \n')
+        file.write('123456789 12345678910 \n')
+        file.write('1234567891011 123456789101112')
+
+    assert get_longest_diverse_words('test_file.txt') == ['123456789101112',
+                                                          '1234567891011',
+                                                          '12345678910',
+                                                          '123456789',
+                                                          '12345678',
+                                                          '1234567',
+                                                          '123456',
+                                                          '12345',
+                                                          '1234',
+                                                          '123']
+
+
+def test_get_rarest_char():
+
+    with open('test_file.txt', mode='w', encoding='unicode-escape') as file:
+        file.write('qwerty qwerty qwerty z')
+
+    assert get_rarest_char('test_file.txt') == 'z'
+
+
+def test_count_punctuation_chars():
+
+    with open('test_file.txt', mode='w', encoding='unicode-escape') as file:
+        file.write('qwerty /  .  , qwerty ()')
+
+    assert count_punctuation_chars('test_file.txt') == 5
+
+
+def test_count_non_ascii_chars():
+
+    with open('test_file.txt', mode='w', encoding='unicode-escape') as file:
+        for ord_ in range(120, 135):
+            file.writelines(chr(ord_))
+
+    assert count_non_ascii_chars('test_file.txt') == 7
+
+
+def test_get_most_common_non_ascii_char():
+
+    with open('test_file.txt', mode='w', encoding='unicode-escape') as file:
+        file.write(chr(130))
+        for ord_ in range(120, 135):
+            file.writelines(chr(ord_))
+
+    assert get_most_common_non_ascii_char('test_file.txt') == chr(130)
