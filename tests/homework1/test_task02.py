23fdb7a1a5110dde68f02e97171b0a3955466546
zhuchkov-artem <artem-1102@mail.ru>
2021-11-14 11:07:49 +0300

    Homework # 2 commit correcting

--- tests/homework1/test_task02.py
+++
@@ -1,7 +0,0 @@
-from homework1.task02 import check_fibonacci
-
-
-def test_check_fibonacci():
-    assert check_fibonacci([0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233,
-                            377, 610, 987, 1597, 2584, 4181, 6765])
-    assert not check_fibonacci([0, 1, 1, 2, 6])
