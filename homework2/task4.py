d2f074b0f3456b1e313e35a602163ad9481348f5
ErshovVladislav10M <yersh2000@gmail.com>
2021-11-14 16:07:55 +0300

    Update task4.py

--- homework2/task4.py
+++ homework2/task4.py
@@ -18,10 +18,18 @@ val_2 = cache_func(*some)
 assert val_1 is val_2
 
 """
-from functools import lru_cache
 from typing import Callable
 
 
-@lru_cache
 def cache(func: Callable) -> Callable:
-    return func
+    saved_data = {}
+
+    def helper(*args):
+        if args in saved_data.keys():
+            return saved_data[args]
+        else:
+            val_func = func(*args)
+            saved_data.update([(args, val_func)])
+            return val_func
+
+    return helper
