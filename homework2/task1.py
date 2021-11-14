c1962869eb67a11ff61d2108250f749964e9aac2
ErshovVladislav10M <yersh2000@gmail.com>
2021-11-14 04:36:52 +0300

    Update task1.py

--- homework2/task1.py
+++ homework2/task1.py
@@ -12,7 +12,7 @@ from typing import List
 def get_longest_diverse_words(file_path: str) -> List[str]:
     ans_words = ["" for i in range(10)]
     len_words = [0 for i in range(10)]
-    with open(file_path) as fi:
+    with open(file_path, encoding="unicode-escape") as fi:
         for line in fi:
             words = line.split()
             for word in words:
@@ -26,7 +26,7 @@ def get_longest_diverse_words(file_path: str) -> List[str]:
 
 def get_rarest_char(file_path: str) -> str:
     dict_letters = {}
-    with open(file_path) as fi:
+    with open(file_path, encoding="unicode-escape") as fi:
         for line in fi:
             for letter in line:
                 if dict_letters.get(letter, -1) == -1:
@@ -47,7 +47,7 @@ def get_rarest_char(file_path: str) -> str:
 def count_punctuation_chars(file_path: str) -> int:
     """I count ascii characters with codes from 33 to 47"""
     counter = 0
-    with open(file_path + ".txt") as fi:
+    with open(file_path, encoding="unicode-escape") as fi:
         for line in fi:
             for i in line:
                 if 48 > ord(i) > 32:
@@ -56,8 +56,42 @@ def count_punctuation_chars(file_path: str) -> int:
 
 
 def count_non_ascii_chars(file_path: str) -> int:
-    ...
+    ans = 0
+    dict_letters = {}
+    with open(file_path, encoding="unicode-escape") as fi:
+        for line in fi:
+            for letter in line:
+                if dict_letters.get(letter, -1) == -1:
+                    dict_letters.update([(letter, 1)])
+                else:
+                    dict_letters[letter] += 1
+
+    while len(dict_letters) > 0:
+        letter, number = dict_letters.popitem()
+        if letter.isascii():
+            continue
+        else:
+            ans += number
+
+    return ans
 
 
 def get_most_common_non_ascii_char(file_path: str) -> str:
-    ...
+    dict_letters = {}
+    with open(file_path, encoding="unicode-escape") as fi:
+        for line in fi:
+            for letter in line:
+                if dict_letters.get(letter, -1) == -1:
+                    dict_letters.update([(letter, 1)])
+                else:
+                    dict_letters[letter] += 1
+
+    print(dict_letters)
+    max_letter, max_number = dict_letters.popitem()
+    while len(dict_letters) > 0:
+        letter, number = dict_letters.popitem()
+        if number > max_number:
+            max_number = number
+            max_letter = letter
+
+    return max_letter
