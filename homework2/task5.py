6980d01dc1db909c49b1ba85780c111101a795ce
ErshovVladislav10M <yersh2000@gmail.com>
2021-11-14 15:47:59 +0300

    Update task5.py

--- homework2/task5.py
+++ homework2/task5.py
@@ -9,9 +9,12 @@ it behaves as range function:
 import string
 
 
-assert = custom_range(string.ascii_lowercase, 'g') == ['a', 'b', 'c', 'd', 'e', 'f']
-assert = custom_range(string.ascii_lowercase, 'g', 'p') == ['g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o']
-assert = custom_range(string.ascii_lowercase, 'p', 'g', -2) == ['p', 'n', 'l', 'j', 'h']
+assert = custom_range(string.ascii_lowercase, 'g') ==
+['a', 'b', 'c', 'd', 'e', 'f']
+assert = custom_range(string.ascii_lowercase, 'g', 'p') ==
+['g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o']
+assert = custom_range(string.ascii_lowercase, 'p', 'g', -2) ==
+['p', 'n', 'l', 'j', 'h']
 
 """
 from typing import Any, List
