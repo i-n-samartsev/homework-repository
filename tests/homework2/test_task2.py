5a984d6bd1fd590e582e02191ff2de289c35116e
ErshovVladislav10M <yersh2000@gmail.com>
2021-11-15 21:44:01 +0300

    Update test_task2.py

--- tests/homework2/test_task2.py
+++ tests/homework2/test_task2.py
@@ -6,7 +6,7 @@ from homework2.task2 import major_and_minor_elem
 @pytest.mark.parametrize(
     "input_data, output_data",
     [
-        ([3, 2, 3], (3, 2),
+        ([3, 2, 3], (3, 2)),
         ([2,2,1,1,1,2,2], (2, 1))
     ],
 )
