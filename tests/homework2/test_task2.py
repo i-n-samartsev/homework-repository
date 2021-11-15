9d7cfd02637828b356818fb9e8ec0acb794a9487
ErshovVladislav10M <yersh2000@gmail.com>
2021-11-15 21:46:14 +0300

    Update test_task2.py

--- tests/homework2/test_task2.py
+++ tests/homework2/test_task2.py
@@ -7,7 +7,7 @@ from homework2.task2 import major_and_minor_elem
     "input_data, output_data",
     [
         ([3, 2, 3], (3, 2)),
-        ([2,2,1,1,1,2,2], (2, 1))
+        ([2, 2, 1, 1, 1, 2, 2], (2, 1))
     ],
 )
 def test_positive(input_data, output_data):
