0aa3bc95f4927ae0d97eda65eae83d24c73ccbc9
ErshovVladislav10M <yersh2000@gmail.com>
2021-11-15 21:40:15 +0300

    Create test_task2.py

---
+++ tests/homework2/test_task2.py
@@ -0,0 +1,14 @@
+import pytest
+
+from homework2.task2 import major_and_minor_elem
+
+
+@pytest.mark.parametrize(
+    "input_data, output_data",
+    [
+        ([3, 2, 3], (3, 2),
+        ([2,2,1,1,1,2,2], (2, 1))
+    ],
+)
+def test_positive(input_data, output_data):
+    assert major_and_minor_elem(input_data) == *output_data
