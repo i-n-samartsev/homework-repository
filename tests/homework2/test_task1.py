71f3cb3fcdbb3c7d710a6b748134a17cedc6b0b5
ErshovVladislav10M <yersh2000@gmail.com>
2021-11-16 04:26:44 +0300

    Update test_task1.py

--- tests/homework2/test_task1.py
+++ tests/homework2/test_task1.py
@@ -1,12 +1,8 @@
 import os
 
-from homework2.task1 import (
-    count_non_ascii_chars,
-    count_punctuation_chars,
-    get_longest_diverse_words,
-    get_most_common_non_ascii_char,
-    get_rarest_char,
-)
+from homework2.task1 import (count_non_ascii_chars, count_punctuation_chars,
+                             get_longest_diverse_words,
+                             get_most_common_non_ascii_char, get_rarest_char)
 
 
 def test_get_longest_diverse_words():
