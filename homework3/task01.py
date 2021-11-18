43bae97578506ad955a2d5f2eed72e3e5c0d5710
ErshovVladislav10M <yersh2000@gmail.com>
2021-11-18 18:38:05 +0300

    Update task01.py

--- homework3/task01.py
+++ homework3/task01.py
@@ -1,15 +1,50 @@
+"""
+In previous homework task 4, you wrote a cache function that remembers other
+function output value.
+Modify it to be a parametrized decorator, so that the following code::
+
+    @cache(times=3)
+    def some_function():
+        pass
+
+
+Would give out cached value up to `times` number only.
+Example::
+
+    @cache(times=2)
+    def f():
+        return input('? ')   
+        # careful with input() in python2, use raw_input() instead
+
+    >>> f()
+    ? 1
+    '1'
+    >>> f()     # will remember previous value
+    '1'
+    >>> f()     # but use it up to two times only
+    '1'
+    >>> f()
+    ? 2
+    '2'
+"""
+
+
 from typing import Callable
 
 
-def cache(func: Callable) -> Callable:
-    saved_data = {}
+def decorator_cache(times: int) -> Callable:
+    
+    def cache(func: Callable) -> Callable:
+        saved_data = {}
 
-    def helper(*args):
-        if args in saved_data.keys():
-            return saved_data[args]
-        else:
-            val_func = func(*args)
-            saved_data.update([(args, val_func)])
-            return val_func
+        def helper(*args):
+            if args in saved_data.keys():
+                return saved_data[args]
+            else:
+                val_func = func(*args)
+                saved_data.update([(args, val_func)])
+                return val_func
 
-    return helper
+        return helper
+
+    return cache
