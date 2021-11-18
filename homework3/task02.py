f78b605b51862a22e41dcd3bec037fb1a9e4824d
ErshovVladislav10M <yersh2000@gmail.com>
2021-11-18 18:58:39 +0300

    Update task02.py

--- homework3/task02.py
+++ homework3/task02.py
@@ -18,3 +18,14 @@ Calculation time should not take more than a minute. Use functional
 capabilities of multiprocessing module.
 You are not allowed to modify slow_calculate function.
 """
+import time
+import struct
+import random
+import hashlib
+
+
+def slow_calculate(value):
+    """Some weird voodoo magic calculations"""
+    time.sleep(random.randint(1, 3))
+    data = hashlib.md5(str(value).encode()).digest()
+    return sum(struct.unpack("<" + "B" * len(data), data))
