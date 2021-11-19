06e0efc6937a62d9a0e021c46063c4ea40e3456e
ErshovVladislav10M <yersh2000@gmail.com>
2021-11-19 16:24:04 +0300

    create test_task03

---
+++ tests/homework3/test_task03.py
@@ -0,0 +1,21 @@
+from homework3.task03 import make_filter, Filter
+
+
+sample_data = [
+    {
+        "name": "Bill",
+        "last_name": "Gilbert",
+        "occupation": "was here",
+        "type": "person",
+    },
+    {"is_dead": True, "kind": "parrot", "type": "bird", "name": "polly"},
+]
+
+
+def test_positive(input_data, output_data):
+    assert make_filter(name="polly", type="bird").apply(sample_data) == {
+        "is_dead": True,
+        "kind": "parrot",
+        "type": "bird",
+        "name": "polly",
+    }
