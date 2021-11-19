90c21814351d6c261dd9dfeac9b8734fbf06c639
zhuchkov-artem <artem-1102@mail.ru>
2021-11-19 23:33:50 +0300

    Homework # 4 initialization

---
+++ homework4/task_4_doctest.py
@@ -0,0 +1,26 @@
+"""
+Write a function that takes a number N as an input and returns N FizzBuzz numbers*
+Write a doctest for that function.
+
+Definition of done:
+ - function is created
+ - function is properly formatted
+ - function has doctests
+ - doctests are run with pytest command
+
+You will learn:
+ - the most common test task for developers
+ - how to write doctests
+ - how to run doctests
+
+
+assert fizzbuzz(5) == ["1", "2", "fizz", "4", "buzz"]
+
+* https://en.wikipedia.org/wiki/Fizz_buzz
+** Энциклопедия профессора Фортрана page 14, 15, "Робот Фортран, чисть картошку!"
+"""
+from typing import List
+
+
+def fizzbuzz(n: int) -> List[str]:
+    pass
