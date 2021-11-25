2638530007e226099df07f0046c77a5bfb347b80
ErshovVladislav10M <yersh2000@gmail.com>
2021-11-25 22:31:35 +0300

    Update save_original_info.py

--- homework5/save_original_info.py
+++ homework5/save_original_info.py
@@ -6,12 +6,14 @@
 print_result изменять нельзя, за исключением добавления вашего
 декоратора на строку отведенную под него - замените комментарий
 
-До применения вашего декоратор будет вызываться AttributeError при custom_sum.__original_func
+До применения вашего декоратор будет вызываться AttributeError
+при custom_sum.__original_func
 Это корректное поведение
 После применения там должна быть исходная функция
 
 Ожидаемый результат:
-print(custom_sum.__doc__)  # 'This function can sum any objects which have __add___'
+print(custom_sum.__doc__)
+# 'This function can sum any objects which have __add___'
 print(custom_sum.__name__)  # 'custom_sum'
 print(custom_sum.__original_func)  # <function custom_sum at <some_id>>
 """
