12a6752bedf907b2797810e4960e43d5ded4f58f
ErshovVladislav10M <yersh2000@gmail.com>
2021-11-25 18:45:12 +0300

    create oop_1

---
+++ homework5/oop_1.py
@@ -0,0 +1,85 @@
+"""
+Необходимо создать 3 класса и взаимосвязь между ними (Student, Teacher,
+Homework)
+Наследование в этой задаче использовать не нужно.
+Для работы с временем использовать модуль datetime
+
+1. Homework принимает на вход 2 атрибута: текст задания и количество дней
+на это задание
+Атрибуты:
+    text - текст задания
+    deadline - хранит объект datetime.timedelta с количеством
+    дней на выполнение
+    created - c точной датой и временем создания
+Методы:
+    is_active - проверяет не истекло ли время на выполнение задания,
+    возвращает boolean
+
+2. Student
+Атрибуты:
+    last_name
+    first_name
+Методы:
+    do_homework - принимает объект Homework и возвращает его же,
+    если задание уже просрочено, то печатет 'You are late' и возвращает None
+
+3. Teacher
+Атрибуты:
+     last_name
+     first_name
+Методы:
+    create_homework - текст задания и количество дней на это задание,
+    возвращает экземпляр Homework
+    Обратите внимание, что для работы этого метода не требуется сам объект.
+
+PEP8 соблюдать строго.
+Всем перечисленным выше атрибутам и методам классов сохранить названия.
+К названием остальных переменных, классов и тд. подходить ответственно -
+давать логичные подходящие имена.
+"""
+import datetime
+
+
+class Homework:
+    text = ""
+    deadline = 0
+    created = 0
+
+    def __init__(self, text, num_of_days):
+        self.text = text
+        self.deadline = datetime.timedelta(num_of_days)
+        self.created = datetime.date.today()
+
+    def is_active(self):
+        if self.created + self.deadline < datetime.date.today():
+            return False
+        else:
+            return True
+
+
+class Student:
+    last_name = ""
+    first_name = ""
+
+    def __init__(self, last_name, first_name):
+        self.last_name = last_name
+        self.first_name = first_name
+
+    def do_homework(self, homework: Homework):
+        if homework.created + homework.deadline > datetime.date.today():
+            return homework
+        else:
+            print("You are late")
+            return None
+
+
+class Teacher:
+    last_name = ""
+    first_name = ""
+
+    def __init__(self, last_name, first_name):
+        self.last_name = last_name
+        self.first_name = first_name
+
+    def create_homework(self, text, num_of_days):
+        return Homework(text, num_of_days)
