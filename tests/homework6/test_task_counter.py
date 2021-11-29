0dd574e9de48920fcdecc2a6f8c68e4e504a95a5
ErshovVladislav10M <yersh2000@gmail.com>
2021-11-29 22:06:19 +0300

    Update test_task_counter.py

--- tests/homework6/test_task_counter.py
+++ tests/homework6/test_task_counter.py
@@ -7,7 +7,7 @@ class User:
 
 
 def test_instances_counter():
-    user, _, _ = User(), User(), User()
+    _, _, _ = User(), User(), User()
     assert (
         User.get_created_instances() == 3
         and User.reset_instances_counter() == 3
