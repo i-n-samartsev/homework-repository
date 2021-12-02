6e416f78fbbd37b207f593db0940d2a3f0984056
ErshovVladislav10M <yersh2000@gmail.com>
2021-12-02 19:19:37 +0300

    Update test_task_oop.py

--- tests/homework5/test_task_oop.py
+++ tests/homework5/test_task_oop.py
@@ -21,11 +21,8 @@ def test_create_homework():
 def test_use_method():
     teacher = Teacher("Daniil", "Shadrin")
     student = Student("Roman", "Petrov")
-
     expired_homework = teacher.create_homework("Learn functions", 0)
-
     oop_homework = teacher.create_homework("create 2 simple classes", 5)
-
     assert (
         student.do_homework(oop_homework) == oop_homework
         and student.do_homework(expired_homework) != expired_homework
