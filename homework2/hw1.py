bfdbd57818a7e3797438f6931acaefc75b090b68
zhuchkov-artem <artem-1102@mail.ru>
2021-12-02 17:53:28 +0300

    Homework # 2 corrected (№ 2)

--- homework2/hw1.py
+++ homework2/hw1.py
@@ -47,7 +47,7 @@ def get_longest_diverse_words(file_path: str, encoding: str = 'utf8',
     words_stat = {}
 
     for word in get_word(file_path, encoding=encoding, errors=errors):
-        words_stat[len(word)] = words_stat.get(len(word), []).append(word)
+        words_stat[len(word)] = words_stat.get(len(word), []) + [word]
 
     sorted_length = sorted(words_stat, reverse=True)
     longest_words = []
