516b7e1a77539366c94cc5d08d1842c682da2ddc
zhuchkov-artem <artem-1102@mail.ru>
2021-12-09 17:52:09 +0300

    Homework # 4 corrected (paths to patch and temporary files)

--- tests/homework4/test_task_1.py
+++ tests/homework4/test_task_1.py
@@ -1,4 +1,5 @@
 import os
+from tempfile import NamedTemporaryFile
 
 import pytest
 
@@ -6,49 +7,61 @@ from homework4.task_1_read_file import (FileDoesNotExist, FileIsEmpty,
                                         MagicNumberError, read_magic_number)
 
 
-@pytest.fixture()
+@pytest.fixture
 def file_with_1_in_first_line():
-    with open(file='test_file.txt', mode='w', encoding='utf8') as file:
-        file.write('1')
-    yield
-    os.remove('test_file.txt')
+    try:
+        with NamedTemporaryFile(mode='w', encoding='utf8',
+                                delete=False) as file:
+            file.write('1')
+        yield file.name
+    finally:
+        os.remove(file.name)
 
 
 @pytest.fixture()
 def file_with_3_in_first_line():
-    with open(file='test_file.txt', mode='w', encoding='utf8') as file:
-        file.write('3')
-    yield
-    os.remove('test_file.txt')
+    try:
+        with NamedTemporaryFile(mode='w', encoding='utf8',
+                                delete=False) as file:
+            file.write('3')
+        yield file.name
+    finally:
+        os.remove(file.name)
 
 
 @pytest.fixture()
 def file_with_abacab_in_first_line():
-    with open(file='test_file.txt', mode='w', encoding='utf8') as file:
-        file.write('abacab')
-    yield
-    os.remove('test_file.txt')
+    try:
+        with NamedTemporaryFile(mode='w', encoding='utf8',
+                                delete=False) as file:
+            file.write('abacab')
+        yield file.name
+    finally:
+        os.remove(file.name)
 
 
 @pytest.fixture()
 def empty_file():
-    with open(file='test_file.txt', mode='w', encoding='utf8'):
-        pass
-    yield
-    os.remove('test_file.txt')
+    try:
+        with NamedTemporaryFile(mode='w', encoding='utf8',
+                                delete=False) as file:
+            pass
+        yield file.name
+    finally:
+        os.remove(file.name)
 
 
 def test_read_magic_number_return_true(file_with_1_in_first_line):
-    assert read_magic_number('test_file.txt') is True
+    assert read_magic_number(file_with_1_in_first_line) is True
 
 
 def test_read_magic_number_return_false(file_with_3_in_first_line):
-    assert read_magic_number('test_file.txt') is False
+    assert read_magic_number(file_with_3_in_first_line) is False
 
 
 def test_read_magic_number_return_magic_error(file_with_abacab_in_first_line):
     with pytest.raises(MagicNumberError):
-        read_magic_number('test_file.txt')
+        read_magic_number(file_with_abacab_in_first_line)
 
 
 def test_read_magic_number_return_invalid_path_exception():
@@ -58,4 +71,4 @@ def test_read_magic_number_return_invalid_path_exception():
 
 def test_read_magic_number_return_file_is_empty_exception(empty_file):
     with pytest.raises(FileIsEmpty):
-        read_magic_number('test_file.txt')
+        read_magic_number(empty_file)
--- tests/homework4/test_task_2.py
+++ tests/homework4/test_task_2.py
@@ -1,7 +1,9 @@
 from unittest.mock import patch
+from urllib.error import URLError
 
 import pytest
 
+import homework4.task_2_mock_input
 from homework4.task_2_mock_input import (DotsOnIError, NetworkClient,
                                          count_dots_on_i)
 
@@ -27,19 +29,22 @@ def test_count_dots_on_i_raise_exception_on_wrong_url():
         count_dots_on_i('https://abacab.abacab/')
 
 
-def test_count_dots_on_i_with_patch_object_return_correct_answer():
-    with patch.object(NetworkClient, 'html', FakeNetworkClient.html):
+def test_count_dots_on_i_with_patching_client_method_return_correct_answer():
+    with patch.object(NetworkClient,
+                      'html',
+                      FakeNetworkClient.html):
         assert count_dots_on_i('https://github.com/zhuchkov-artem') == 1
 
 
-def test_count_dots_on_i_with_patch_return_correct_answer():
-    with patch('homework4.task_2_mock_input.NetworkClient',
-               new=FakeNetworkClient):
+def test_count_dots_on_i_with_patching_full_client_return_correct_answer():
+    with patch.object(homework4.task_2_mock_input,
+                      'NetworkClient',
+                      FakeNetworkClient):
         assert count_dots_on_i('https://github.com/zhuchkov-artem') == 1
 
 
-def test_count_dots_on_i_with_patch_raise_exception_on_correct_url():
-    with patch('homework4.task_2_mock_input.NetworkClient',
-               side_effect=DotsOnIError):
+def test_count_dots_on_i_with_patch_urlopen_raise_exception():
+    with patch.object(homework4.task_2_mock_input, 'urlopen',
+                      side_effect=URLError('Incorrect URL')):
         with pytest.raises(DotsOnIError):
             count_dots_on_i('https://example.com/')
