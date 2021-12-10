65fd2831817348e1ba9ca2561db3dff96396978c
zhuchkov-artem <artem-1102@mail.ru>
2021-12-10 13:00:49 +0300

    Task 2 (added second version of the function with generator)

--- homework7/hw2.py
+++ homework7/hw2.py
@@ -18,6 +18,26 @@ Examples:
     Explanation: s becomes "c" while t becomes "b".
 
 """
+from typing import Generator
+from timeit import timeit
+from functools import partial
+
+
+def reversed_converted_word(word: str) -> Generator:
+    """
+        Converts a string, considering that # is a backspace character.
+        Returns reversed result string.
+    """
+    backspaces = 0
+    word = iter(reversed(word))
+    for char in word:
+        if char == '#':
+            backspaces += 1
+        else:
+            if backspaces:
+                backspaces -= 1
+                continue
+            yield char
 
 
 def convert_word(word: str) -> str:
@@ -39,3 +59,22 @@ def backspace_compare(first: str, second: str) -> bool:
         empty text editors. # means a backspace character.
     """
     return convert_word(first) == convert_word(second)
+
+
+def backspace_compare_with_generator(first: str, second: str) -> bool:
+    """
+        Takes two strings. Return if they are equal when both are typed into
+        empty text editors. # means a backspace character.
+    """
+    first, second = map(reversed_converted_word, (first, second))
+    return ''.join(first) == ''.join(second)
+
+
+if __name__ == '__main__':
+    words = ('ab#c' * 10000, 'ad#c' * 10000)
+    backspace_compare = partial(backspace_compare, *words)
+    backspace_compare_with_generator = partial(
+        backspace_compare_with_generator, *words)
+
+    print(timeit(backspace_compare, number=100))
+    print(timeit(backspace_compare_with_generator, number=100))
--- tests/homework7/test_hw2.py
+++ tests/homework7/test_hw2.py
@@ -1,17 +1,21 @@
-from homework7.hw2 import convert_word
+from homework7.hw2 import backspace_compare, backspace_compare_with_generator
 
 
-def test_convert_word_strings_with_single_backspace_are_equal():
-    assert convert_word('ab#c') == convert_word('ad#c')
+def test_backspace_compare_strings_with_single_backspace_are_equal():
+    assert backspace_compare('ab#c', 'ad#c') is True
+    assert backspace_compare_with_generator('ab#c', 'ad#c') is True
 
 
-def test_convert_word_strings_with_double_backspace_are_equal():
-    assert convert_word('a##c') == convert_word('#a#c')
+def test_backspace_compare_strings_with_double_backspace_are_equal():
+    assert backspace_compare('abcd#e###c', '#aa#c') is True
+    assert backspace_compare_with_generator('abcd#e###c', '#aa#c') is True
 
 
-def test_convert_word_strings_with_single_backspace_are_not_equal():
-    assert convert_word('a#c') != convert_word('b')
+def test_backspace_compare_strings_with_single_backspace_are_not_equal():
+    assert backspace_compare('a#c', 'b') is False
+    assert backspace_compare_with_generator('a#c', 'b') is False
 
 
-def test_convert_word_empty_strings_are_equal():
-    assert convert_word('###') == convert_word('')
+def test_backspace_compare_empty_strings_are_equal():
+    assert backspace_compare('###', '') is True
+    assert backspace_compare_with_generator('###', '') is True
