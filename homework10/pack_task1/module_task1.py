d7d24f63a88641d2dc3ab3da9fb223ea18a0b846
alex <allexander0509@gmail.com>
2021-12-23 17:35:20 +0300

    Homework10 task1 files and definition added

---
+++ homework10/pack_task1/module_task1.py
@@ -0,0 +1,11 @@
+"""
+    Ваша задача спарсить информацию о компаниях, находящихся в индексе S&P 500 с данного сайта:
+    https://markets.businessinsider.com/index/components/s&p_500
+
+    Подробно про задание:
+    https://github.com/lex-av/epam_python_autumn_2020/blob/main/lecture_10_parallelization/hw/stonks.md
+"""
+
+
+if __name__ == "__main__":
+    ...
---
+++ homework10/tests/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+
+def functionality():
+    return
+
+
+if __name__ == "__main__":
+    functionality()
