b0dc83ffdd7ad261a8d8370f89ce79ff88d1430b
zhuchkov-artem <artem-1102@mail.ru>
2021-12-25 21:06:19 +0300

    Task 1 corrected (added custom merge)

--- homework9/hw1.py
+++ homework9/hw1.py
@@ -21,6 +21,11 @@ from typing import Iterator, List, Union
 
 class NotAnIntegerError(ValueError):
     """Exception class if file contains not only integers"""
+    def __init__(self, message=''):
+        self.message = message
+
+    def __str__(self):
+        return f'{self.message} is not an integer'
 
 
 def get_integers_from_file(filename: str) -> Iterator:
@@ -28,12 +33,30 @@ def get_integers_from_file(filename: str) -> Iterator:
         for line_number, integer in enumerate(file, 1):
             try:
                 yield int(integer.strip())
-            except ValueError as err:
-                raise NotAnIntegerError(f'{filename=} {line_number=} is not '
-                                        f'an integer') from err
+            except ValueError:
+                raise NotAnIntegerError(f'{filename=} {line_number=}')
 
 
 def merge_sorted_files(file_list: List[Union[Path, str]]) -> Iterator:
 
+    file_iterators = map(get_integers_from_file, file_list)
+    iter_row = {}
+    for iterator in file_iterators:
+        value = next(iterator, None)
+        if value is not None:
+            iter_row[iterator] = value
+    while iter_row:
+        next_iterator = min(iter_row, key=iter_row.get)
+        yield iter_row[next_iterator]
+        value = next(next_iterator, None)
+        if value is not None:
+            iter_row[next_iterator] = value
+        else:
+            iter_row.pop(next_iterator)
+
+# Another solution with heapq.merge
+
+
+def merge_sorted_files_with_heapq(file_list) -> Iterator:
     file_iterators = map(get_integers_from_file, file_list)
     yield from merge(*file_iterators)
--- tests/homework9/test_hw1.py
+++ tests/homework9/test_hw1.py
@@ -4,6 +4,7 @@ from tempfile import NamedTemporaryFile
 from pytest import fixture, raises
 
 from homework9.hw1 import merge_sorted_files
+from homework9.hw1 import merge_sorted_files_with_heapq as merge_with_heapq
 
 
 @fixture
@@ -51,19 +52,24 @@ def texts_with_non_integers():
 def test_merge_sorted_equal_length_files(file_creator):
     equal_length_files = file_creator(equal_length_texts())
     assert list(merge_sorted_files(equal_length_files)) == list(range(-4, 5))
+    assert list(merge_with_heapq(equal_length_files)) == list(range(-4, 5))
 
 
 def test_merge_sorted_different_length_files(file_creator):
     diff_length_files = file_creator(diff_length_texts())
     assert list(merge_sorted_files(diff_length_files)) == list(range(-4, 5))
+    assert list(merge_with_heapq(diff_length_files)) == list(range(-4, 5))
 
 
 def test_merge_sorted_empty_files(file_creator):
     empty_files = file_creator(empty_texts())
     assert list(merge_sorted_files(empty_files)) == []
+    assert list(merge_with_heapq(empty_files)) == []
 
 
 def test_merge_sorted_non_integers_files_raises_error(file_creator):
     files_with_non_integers = file_creator(texts_with_non_integers())
     with raises(ValueError, match='is not an integer'):
         list(merge_sorted_files(files_with_non_integers))
+    with raises(ValueError, match='is not an integer'):
+        list(merge_with_heapq(files_with_non_integers))
