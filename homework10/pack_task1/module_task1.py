c4a9cb338ce10e392f3906e3c140e662a1dbf2cc
alex <allexander0509@gmail.com>
2021-12-26 16:33:45 +0300

    Homework10 task1 corp links parser finished multithreading implementation

--- homework10/pack_task1/module_task1.py
+++ homework10/pack_task1/module_task1.py
@@ -7,5 +7,54 @@
 """
 
 
+from collections import namedtuple
+from multiprocessing.pool import ThreadPool
+
+import requests
+from bs4 import BeautifulSoup
+
+
+class CorpoUrlsGetter:
+    """Container to get and store corps pre-names, urls and year percent"""
+
+    def __init__(self, url="https://markets.businessinsider.com/index/components/s&p_500?p=", pages_count=11):
+        self.domain = "https://markets.businessinsider.com"
+        self.pages_count = pages_count
+        self.pages_list = [url + str(page_number) for page_number in range(1, self.pages_count + 1)]
+        self.corp_table = []
+        self.table_builder_threading()
+
+    def append_page_data(self, url):
+        """Parse table on the page for urls and percentages"""
+        soup = BeautifulSoup(requests.get(url).text, "lxml")
+        page_data = []
+        Corp = namedtuple("Corp", "url percent")  # Corp._make([])
+        link_cells = soup.find("table", class_="table__layout--fixed").find_all("td", "table__td--big")
+        rows = soup.find("table", class_="table__layout--fixed").find_all("tr")[1:]  # skip table header
+
+        links = []
+        percentages = []
+
+        for cell in link_cells:
+            links.append(self.domain + cell.find("a").get("href"))
+
+        for row in rows:
+            percentage_cell = row.find_all("td")[7]
+            cell_value = percentage_cell.find_all("span")[0]
+            percentage_value = cell_value.contents[0]
+            percentages.append(percentage_value)
+
+        for link, percentage in zip(links, percentages):
+            page_data.append(Corp._make([link, percentage]))
+
+        self.corp_table += page_data
+
+    def table_builder_threading(self):
+        with ThreadPool(self.pages_count) as t_pool:
+            t_pool.map(self.append_page_data, self.pages_list)
+
+
 if __name__ == "__main__":
-    ...
+
+    new_table = CorpoUrlsGetter()
+    print()
--- homework10/pack_task1/utils_task1.py
+++ homework10/pack_task1/utils_task1.py
@@ -1,5 +1,56 @@
 # -*- coding: utf-8 -*-
 
+import asyncio
+import ssl
+
+import aiohttp
+import requests
+from bs4 import BeautifulSoup
+
+url_list = [
+    "https://api.pushshift.io/reddit/search/comment/?q=Nestle&size=30&after=1530396000&before=1530436000",
+    "https://api.pushshift.io/reddit/search/comment/?q=Nestle&size=30&after=1530436000&before=1530476000",
+]
+
+
+async def fetch(session, url):
+    async with session.get(url, ssl=ssl.SSLContext()) as response:
+        return await response.json()
+
+
+async def fetch_all(urls, loop):
+    async with aiohttp.ClientSession(loop=loop) as session:
+        results = await asyncio.gather(*[fetch(session, url) for url in urls], return_exceptions=True)
+        return results
+
 
 if __name__ == "__main__":
-    ...
+    page = requests.get("https://markets.businessinsider.com/index/components/s&p_500?p=1")
+
+    soup = BeautifulSoup(page.text, "lxml")
+
+    tds = soup.find("table", class_="table__layout--fixed").find_all("td", "table__td--big")
+    pgs = soup.find("div", class_="finando_paging margin-top--small").find_all("a")
+    pers = soup.find("table", class_="table__layout--fixed").find_all("tr")
+
+    companies = []
+    comp_links = []
+    persentages = []
+    pages = []
+    pages_links = []
+
+    for td in tds:
+        companies.append(td.find("a").get("title"))
+        comp_links.append(td.find("a").get("href"))
+
+    for page in pgs:
+        pages_links.append(page.get("href"))
+
+    # table_columns = soup.find_all("tr")[0].find_all("th")
+    #
+    # for tr in soup.find_all("tr")[1:]:
+    #     tds = tr.find_all("td")
+    #     vals = [tds[0].text.strip(), tds[1].text.strip(), tds[2].text.strip()]
+    #     print(tds[0].text, "  ", tds[1].text, "  ", tds[2].text)
+
+    print()
