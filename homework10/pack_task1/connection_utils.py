c26822b36080f12c2a4e08ccfc862c408221d057
alex <allexander0509@gmail.com>
2021-12-29 00:20:34 +0300

    Homework10 task1 refactor: some functionality was put to connection_utils module, divided requesting and parsing logic, implemented multiprocessing in parsing logic

---
+++ homework10/pack_task1/connection_utils.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+
+import re
+
+import requests
+from bs4 import BeautifulSoup, SoupStrainer
+
+
+def float_normalizer(value_in_str):
+    if "," in value_in_str:
+        lst_repr = value_in_str.strip("%").split(",")
+        return float("".join(lst_repr))
+
+    return float(value_in_str.strip("%"))
+
+
+def get_base_response(url="https://markets.businessinsider.com/index/components/s&p_500"):
+    """Returns response of base web-page needed for s&p500 list page counting"""
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response
+
+
+def get_page_count(response, fixed_page_count=None):
+    """Returns s&p500 list pages count"""
+    if fixed_page_count:
+        return fixed_page_count
+
+    soup = BeautifulSoup(response.text, "lxml")
+    div_class = "finando_paging margin-top--small"
+    page_count = soup.find("div", class_=div_class).contents[19].contents[0]
+
+    return int(page_count)
+
+
+def get_page_links(page_count=1, base_link="https://markets.businessinsider.com/index/components/s&p_500?p="):
+    return [base_link + str(num) for num in range(1, page_count + 1)]
+
+
+def urls_growth_parse(text_data):
+    base_url = "https://markets.businessinsider.com"
+    soup = BeautifulSoup(text_data, "lxml")
+    url_cells = soup.find("table", class_="table__layout--fixed").find_all("td", "table__td--big")
+    table_rows = soup.find("table", class_="table__layout--fixed").find_all("tr")[1:]  # skip table header
+
+    urls = []
+    growth_percentages = []
+
+    for cell in url_cells:
+        urls.append(base_url + cell.find("a").get("href"))
+
+    for row in table_rows:
+        percentage_cell = row.find_all("td")[7]
+        cell_value = percentage_cell.find_all("span")[1]
+        growth_percentages.append(float_normalizer(cell_value.contents[0]))
+
+    parsed_data = []
+
+    for url, percentage in zip(urls, growth_percentages):
+        parsed_data.append({"url": url, "year_growth": percentage})
+
+    return parsed_data
+
+
+def corp_name_parse(text_data):
+    limiter = SoupStrainer("span")
+    soup = BeautifulSoup(text_data, "lxml", parse_only=limiter)
+    print("parsed some name")
+    return soup.find("span", class_="price-section__label").contents[0]
+
+
+def corp_code_parse(text_data):
+    limiter = SoupStrainer("span")
+    soup = BeautifulSoup(text_data, "lxml", parse_only=limiter)
+    print("parsed some code")
+    return soup.find("span", class_="price-section__category").find("span").contents[0].strip(",").strip(" ")
+
+
+def corp_price_parse(text_data, rub_rate):
+    limiter = SoupStrainer("div")
+    soup = BeautifulSoup(text_data, "lxml", parse_only=limiter)
+
+    try:
+        price = float_normalizer(soup.find("div", text=re.compile("Open")).parent.text.strip().strip("Open").strip())
+        price = round(rub_rate * price, 2)
+    except AttributeError:
+        price = None
+
+    print("parsed some price")
+    return price
+
+
+def corp_pe_parse(text_data):
+    limiter = SoupStrainer("div")
+    soup = BeautifulSoup(text_data, "lxml", parse_only=limiter)
+
+    try:
+        p_e = float_normalizer(
+            soup.find("div", text=re.compile("P/E Ratio")).parent.text.strip().strip("P/E Ratio").strip()
+        )
+    except AttributeError:
+        p_e = None
+
+    print("parsed some PE")
+    return p_e
+
+
+def corp_max_profit_parse(text_data):
+    limiter = SoupStrainer("div")
+    soup = BeautifulSoup(text_data, "lxml", parse_only=limiter)
+    tag_class_name_low = "snapshot__data-item snapshot__data-item--small"
+    tag_class_name_high = "snapshot__data-item snapshot__data-item--small snapshot__data-item--right"
+
+    try:
+        week_52_low = float_normalizer(soup.find_all("div", class_=tag_class_name_low)[1].contents[0].strip())
+        week_52_high = float_normalizer(soup.find_all("div", class_=tag_class_name_high)[1].contents[0].strip())
+        week_profit = round(((week_52_high - week_52_low) / week_52_low) * 100, 2)
+    except IndexError:
+        week_profit = None
+
+    print("parsed some profits")
+    return week_profit
+
+
+if __name__ == "__main__":
+    pass
--- homework10/pack_task1/module_task1.py
+++ homework10/pack_task1/module_task1.py
@@ -9,126 +9,33 @@
 
 import asyncio
 import json
-import re
 import time
-from multiprocessing.pool import ThreadPool
+from multiprocessing import Pool
 
 import aiohttp
-import requests
+import connection_utils as cu
 import xmltodict
-from bs4 import BeautifulSoup
 
 
-class CorpoUrlsGetter:
-    """
-    Container to get and store corps pre-names, urls and year percent
-    Implements list behavior (slices, len, getitem, iteration)
-    Generates list of dicts with url and company growth percentage
-    """
+class MarketsInsiderRawData:
+    """Class for getting all the data as responses"""
 
-    def __init__(self, url="https://markets.businessinsider.com/index/components/s&p_500?p=", pages_count=11):
-        self.domain = "https://markets.businessinsider.com"
-        self.pages_count = pages_count
-        self.pages_list = [url + str(page_number) for page_number in range(1, self.pages_count + 1)]
-        self.corp_table = []
-        self.table_links = []
-        self.table_cursor = 0
-        self.table_builder_threading()
+    def __init__(self, fixed_page_count=None):
+        self.fixed_page_count = fixed_page_count
 
-    def __getitem__(self, index):
-        if isinstance(index, slice):
-            # Get the start, stop, and step from the slice
-            return [self.corp_table[ii] for ii in range(*index.indices(len(self.corp_table)))]
-        elif isinstance(index, int):
-            if index < len(self.corp_table):
-                return self.corp_table[index]
-            else:
-                raise IndexError("Index is out of range.")
-        else:
-            raise TypeError("Invalid argument type.")
+        self.rub_rate = self._get_rate()
+        self.base_resp = cu.get_base_response()
+        self.page_count = cu.get_page_count(self.base_resp, self.fixed_page_count)
+        self.page_links = cu.get_page_links(self.page_count)
+        self.pages_text_data = self.build_response_list(self.page_links)
 
-    def __next__(self):
-        if len(self.corp_table) > self.table_cursor:
-            value = self.corp_table[self.table_cursor]
-            self.table_cursor += 1
-            return value
-        else:
-            raise StopIteration
+        self.links_growth_data = []
+        for text_data in self.pages_text_data:
+            self.links_growth_data += cu.urls_growth_parse(text_data)
 
-    def __contains__(self, link):
-        if link in self.table_links:
-            return True
-        else:
-            return False
-
-    def __iter__(self):
-        return self
-
-    def __len__(self):
-        return len(self.corp_table)
-
-    @staticmethod
-    def percentage_normalizer(value_in_str):
-        """Converts given str from web-page to float"""
-        if "," in value_in_str:
-            lst_repr = value_in_str.strip("%").split(",")
-            return float("".join(lst_repr))
-
-        return float(value_in_str.strip("%"))
-
-    def append_page_data(self, url):
-        """Parse table on the page for urls and percentages"""
-        soup = BeautifulSoup(requests.get(url).text, "lxml")
-        page_data = []
-        link_cells = soup.find("table", class_="table__layout--fixed").find_all("td", "table__td--big")
-        rows = soup.find("table", class_="table__layout--fixed").find_all("tr")[1:]  # skip table header
-
-        links = []
-        percentages = []
-
-        for cell in link_cells:
-            links.append(self.domain + cell.find("a").get("href"))
-            self.table_links.append(self.domain + cell.find("a").get("href"))
-
-        for row in rows:
-            percentage_cell = row.find_all("td")[7]
-            cell_value = percentage_cell.find_all("span")[1]
-            percentage_value = self.percentage_normalizer(cell_value.contents[0])
-            percentages.append(percentage_value)
-
-        for link, percentage in zip(links, percentages):
-            # page_data.append(Corp._make([link, percentage]))
-            page_data.append({"link": link, "percentage": percentage})
-
-        self.corp_table += page_data
-
-    def table_builder_threading(self):
-        with ThreadPool(self.pages_count) as t_pool:
-            t_pool.map(self.append_page_data, self.pages_list)
-
-
-class CompanyDataTableAsync:
-    """
-    Collection to store S&P-500 corps parameters
-    Gets Urls/percentage table-object from CorpoUrlsGetter
-
-    Every Corp in output table has parameters:
-    code | name | growth | price | PE | max_profit
-    """
-
-    def __init__(self, table_src):
-        self.src_data = table_src
-        self.data_table = []
-        self.rate = self._get_rate()
-        asyncio.run(self._table_builder())
-
-    @staticmethod
-    def _float_normalizer(value_in_str):
-        """Converts given str from web-page to float"""
-        if "," in value_in_str:
-            lst_repr = value_in_str.strip("%").split(",")
-            return float("".join(lst_repr))
-        return float(value_in_str.strip())
+        self.details_links = [corp["url"] for corp in self.links_growth_data]
+        self.growth_values = [corp["year_growth"] for corp in self.links_growth_data]
+        self.details_text_data = self.build_response_list(self.details_links)
 
     def _get_rate(self):
         """Return valute rate for current date"""
@@ -137,113 +44,127 @@ class CompanyDataTableAsync:
         usd = float(rate_data["ValCurs"]["Valute"][10]["Value"].replace(",", "."))
         return usd
 
-    async def _get_req(self):
+    @staticmethod
+    async def _get_req():
         """aiohttp getter for valute rate xml-data from bank api"""
         async with aiohttp.ClientSession() as session:
             async with session.get("http://www.cbr.ru/scripts/XML_daily.asp") as resp:
                 response = await resp.text()
                 return response
 
-    async def _get_corp_data(self, session, url, percentage, rate):
-        """Generates corp data-dict by parsing corp web-page"""
-        async with session.get(url) as response:
-            if response.status != 200:
-                self.data_table.append(
-                    {"code": None, "name": None, "growth": -1, "price": -1, "PE": -1, "max_profit": -1}
-                )
-            else:
-                await asyncio.sleep(0.2)
-                response_text = await response.text()
-                soup = BeautifulSoup(response_text, "lxml")
+    @staticmethod
+    async def get_request_text(session, url):
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                return await resp.text()
 
-                code = (
-                    soup.find("span", class_="price-section__category").find("span").contents[0].strip(",").strip(" ")
-                )
-                name = soup.find("span", class_="price-section__label").contents[0]
-                print(name)  # For debug purpose
+    async def get_multiple_requests_text(self, url_list):
+        async with aiohttp.ClientSession() as session:
+            task_list = []
+            for url in url_list:
+                task = asyncio.create_task(self.get_request_text(session, url))
+                task_list.append(task)
+            return await asyncio.gather(*task_list)
 
-                try:
-                    price = self._float_normalizer(
-                        soup.find("div", text=re.compile("Open")).parent.text.strip().strip("Open").strip()
-                    )
-                    price = round(rate * price, 2)
-                except AttributeError:
-                    price = -1
+    def build_response_list(self, url_list):
+        return asyncio.run(self.get_multiple_requests_text(url_list))
 
-                try:
-                    p_e = self._float_normalizer(
-                        soup.find("div", text=re.compile("P/E Ratio")).parent.text.strip().strip("P/E Ratio").strip()
-                    )
-                except AttributeError:
-                    p_e = -1
 
-                tag_class_name_low = "snapshot__data-item snapshot__data-item--small"
-                tag_class_name_high = "snapshot__data-item snapshot__data-item--small snapshot__data-item--right"
+class CorpDataParser:
+    """Class to process and present data from MarketInsider"""
 
-                try:
-                    week_52_low = self._float_normalizer(
-                        soup.find_all("div", class_=tag_class_name_low)[1].contents[0].strip()
-                    )
-                    week_52_high = self._float_normalizer(
-                        soup.find_all("div", class_=tag_class_name_high)[1].contents[0].strip()
-                    )
-                    week_profit = round(((week_52_high - week_52_low) / week_52_low) * 100, 2)
-                except IndexError:
-                    week_profit = 0
+    def __init__(self, details_text_data, growth_table, cpu_count=6):
+        self.cpu_count = cpu_count
+        self.details_text_data = details_text_data
+        self.growth_table = growth_table
 
-                self.data_table.append(
-                    {
-                        "code": code,
-                        "name": name,
-                        "growth": percentage,
-                        "price": price,
-                        "PE": p_e,
-                        "max_profit": week_profit,
-                    }
-                )
+        self.corp_names = []
+        self.corp_codes = []
+        self.corp_prices = []
+        self.pe_vals = []
+        self.corp_max_profit_vals = []
 
-    async def _table_builder(self):
-        """Async task scheduler"""
-        connector = aiohttp.TCPConnector(limit=500, limit_per_host=200, force_close=False, enable_cleanup_closed=True)
-        async with aiohttp.ClientSession(connector=connector) as session:
-            tasks = []
-            for corp in self.src_data:
-                link = corp["link"]
-                percentage = corp["percentage"]
-                task = asyncio.create_task(self._get_corp_data(session, link, percentage, self.rate))
-                tasks.append(task)
-            await asyncio.gather(*tasks)
+        self.data_table = []
+
+    def compute_data(self):
+        with Pool(self.cpu_count) as p:
+            self.corp_names = p.map(cu.corp_name_parse, self.details_text_data)
+        with Pool(self.cpu_count) as p:
+            self.corp_codes = p.map(cu.corp_code_parse, self.details_text_data)
+        with Pool(self.cpu_count) as p:
+            self.corp_prices = p.starmap(cu.corp_price_parse, zip(self.details_text_data, self.growth_table))
+        with Pool(self.cpu_count) as p:
+            self.pe_vals = p.map(cu.corp_pe_parse, self.details_text_data)
+        with Pool(self.cpu_count) as p:
+            self.corp_max_profit_vals = p.map(cu.corp_max_profit_parse, self.details_text_data)
+
+    def build_data_table(self):
+        for name, code, price, pe, max_profit, growth in zip(
+            self.corp_names,
+            self.corp_codes,
+            self.corp_prices,
+            self.pe_vals,
+            self.corp_max_profit_vals,
+            self.growth_table,
+        ):
+
+            self.data_table.append(
+                {
+                    "code": code,
+                    "name": name,
+                    "price": price,
+                    "growth": growth,
+                    "PE": pe,
+                    "max_profit": max_profit,
+                }
+            )
 
     def generate_top(self, parameter, rev=True, length=10):
-        """Generates top ten corps from data_table by given parameter"""
+        """
+        Generates top ten corps from data_table by given parameter
+        Cleans table from line with None values
+        """
+        remove_indexes = []
+
+        for index, corp_data in enumerate(self.data_table):
+            if corp_data["price"] is None or corp_data["PE"] is None or corp_data["max_profit"] is None:
+                remove_indexes.append(index)
+        for index in sorted(remove_indexes, reverse=True):
+            self.data_table.pop(index)
+
         return sorted(self.data_table, key=lambda row: row[parameter], reverse=rev)[:length]
 
-    def json_writer(self, dict_list, report_name):
+    @staticmethod
+    def json_writer(dict_list, report_name):
         """Generates json-file, based on given report"""
 
         file_name = report_name + ".json"
         with open(file_name, "w") as report:
-            for dict in dict_list:
-                json.dump(dict, report, indent=2)
+            for dct in dict_list:
+                json.dump(dct, report, indent=2)
 
 
 if __name__ == "__main__":
-
-    new_table = CorpoUrlsGetter()
-
     start = time.time()
-    corp_data = CompanyDataTableAsync(new_table)
+    raw_data = MarketsInsiderRawData()
     end = time.time() - start
     print(end)
 
-    top_price = corp_data.generate_top("price", length=10)
-    top_low_pe = corp_data.generate_top("PE", rev=False, length=10)
-    top_growth = corp_data.generate_top("growth", length=10)
-    top_max_profit = corp_data.generate_top("max_profit", length=10)
+    start = time.time()
+    table_data = CorpDataParser(raw_data.details_text_data, raw_data.growth_values)
+    table_data.compute_data()
+    table_data.build_data_table()
+    end = time.time() - start
+    print(end)
 
-    corp_data.json_writer(top_price, "rep1_price")
-    corp_data.json_writer(top_low_pe, "rep2_pe")
-    corp_data.json_writer(top_growth, "rep3_growth")
-    corp_data.json_writer(top_max_profit, "rep4_profit")
+    top_price = table_data.generate_top("price", length=10)
+    top_low_pe = table_data.generate_top("PE", rev=False, length=10)
+    top_growth = table_data.generate_top("growth", length=10)
+    top_max_profit = table_data.generate_top("max_profit", length=10)
+
+    table_data.json_writer(top_price, "rep1_price")
+    table_data.json_writer(top_low_pe, "rep2_pe")
+    table_data.json_writer(top_growth, "rep3_growth")
+    table_data.json_writer(top_max_profit, "rep4_profit")
 
     print()
