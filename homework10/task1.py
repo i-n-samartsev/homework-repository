524b165ba5953957dcd6d8165dd846be4aed6f56
ErshovVladislav10M <yersh2000@gmail.com>
2021-12-29 22:33:33 +0300

    Update task1.py

--- homework10/task1.py
+++ homework10/task1.py
@@ -34,9 +34,140 @@ https://markets.businessinsider.com/index/components/s&p_500
 For scrapping you cans use `beautifulsoup4` <br>
 For requesting `aiohttp`
 """
+import asyncio
+from functools import lru_cache
 
+import aiohttp
+import bs4
+import requests
 
 COMPANIES_LIST_URLS = [
     f"https://markets.businessinsider.com/index/components/s&p_500?p={url}"
     for url in range(1, 11)
 ]
+
+
+@lru_cache(1)
+def get_usd_to_rub() -> float:
+    today = datetime.date.today().strftime("%d/%m/%Y")
+    usd_to_rub = requests.get(
+        "http://www.cbr.ru/scripts/XML_dynamic.asp"
+        f"?date_req1={today}&date_req2={today}&VAL_NM_RQ=R01235"
+    )
+    return float(
+        usd_to_rub.text.split("<Value>")[1].split("<")[0].replace(",", ".")
+    )
+
+
+async def fetch_response(url: str) -> str:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            text = await response.text()
+            return text
+
+
+async def fetch_responses(urls: list[str]) -> list[str]:
+    tasks = [asyncio.create_task(fetch_response(url)) for url in urls]
+    await asyncio.gather(*tasks)
+    return [task.result() for task in tasks]
+
+
+def parse_companies_list(data: str) -> list[dict]:
+    soup = BeautifulSoup(data, "html.parser")
+    table = soup.find("tbody")
+    companies_data = dict()
+    companies_data["name"] = [
+        link.get("title") for link in table.find_all("a")
+    ]
+    companies_data["growth"] = [
+        str(
+            soup.select(
+                f".table__tbody > tr:nth-child({n})"
+                "> td:nth-child(8) > span:nth-child(3)"
+            )
+        )[-14:-9].strip(">")
+        for n, _ in enumerate(soup.find_all("tr"), 1)
+    ]
+    companies_data['url'] = [
+        f"https://markets.businessinsider.com{link.get('href')}"
+        for link in table.find_all("a")
+    ]
+    return [
+        {"name": name, "url": url, "growth": growth}
+        for name, growth, url in zip(*companies_data.values())
+    ]
+
+def get_price(soup: bs4.BeautifulSoup) -> float:
+    try:
+        return round(
+            float(
+                soup.find(
+                    "span", class_="price-section__current-value"
+                ).get_text().replace(",", "")
+            ) * get_usd_to_rub(),
+            2
+        )
+    except AttributeError:
+        return 0
+
+
+def get_high_52(soup: bs4.BeautifulSoup) -> float:
+    try:
+        return float(
+            soup.find(
+                "div", text="52 Week High"
+            ).previous_element.strip().replace(",", "")
+        )
+    except AttributeError:
+        return 0
+
+
+def get_low_52(soup: bs4.BeautifulSoup) -> float:
+    try:
+        return float(
+            soup.find(
+                "div", text="52 Week Low"
+            ).previous_element.strip().replace(",", "")
+        )
+    except AttributeError:
+        return 0
+
+
+def get_p_e(soup: bs4.BeautifulSoup) -> float:
+    try:
+        return float(
+            soup.find(
+                "div", text="P/E Ratio"
+            ).previous_element.strip().replace(",", "")
+        )
+    except AttributeError:
+        return 0
+
+
+def parse_company_page(data: str) -> dict:
+    soup = bs4.BeautifulSoup(data, "html.parser")
+    company_data = dict()
+    company_data["code"] = soup.title.get_text().split()[0]
+    company_data["price"] = get_price(soup)
+    company_data["p_e"] = get_p_e(soup)
+    low_52 = get_low_52(soup)
+    high_52 = get_high_52(soup)
+    company_data["lost_profit"] = round(high_52 - low_52, 2)
+    return company_data
+
+
+async def dict_constructor() -> list[dict]:
+    companies_lists = await fetch_responses(companies_list_urls)
+    companies_dicts = []
+    for companies_list in companies_lists:
+        companies_dicts += parse_companies_list(companies_list)
+
+    companies_urls = [
+        company_data.pop("url") for company_data in companies_dicts
+    ]
+    companies_pages = await fetch_responses(companies_urls)
+    for company_page, company_dict in zip(companies_pages, companies_dicts):
+        company_data = parse_company_page(company_page)
+        company_dict |= company_data
+
+    return companies_dicts
